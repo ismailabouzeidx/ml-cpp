04cadbf262ea614225bd0775c586ab3dcba97e1c
ismail <ismailabouzeidd@gmail.com>
2024-10-05 06:46:21 -0700

    [bug] remove hard-code learning rate and set to that passed into NN constructor

--- src/NN.cpp
+++ src/NN.cpp
@@ -7,6 +7,7 @@ NN::NN(float lr){
 NN::~NN(){}
 
 void NN::add_layer(std::unique_ptr<layer> layer){
+    layer->learning_rate = this->learning_rate;
     this->layers.emplace_back(std::move(layer));
 }
 
@@ -37,30 +38,51 @@ void NN::print_neurons(){
 }
 
 std::vector<float> NN::forward(std::vector<float> &input){
-    std::vector<float> current_input = input;
+    auto current_input = input;
     for (const auto &layer : this->layers){
         current_input = layer->forward(current_input);
     }
     return current_input;
 }
 
+void NN::backward(std::vector<float>& target) {
+    // Calculate initial error at the output layer (direct error)
+     std::vector<float> dL_dO = mse_loss_derivative(target, this->layers.back()->neurons);
 
-
-void NN::train(std::vector<float> &input, std::vector<float> &target, int epochs) {
-    this->init_weights();
-    
-    // Step 2: Training loop for specified epochs
-    for (int epoch = 0; epoch < epochs; ++epoch) {
-        std::cout << "Epoch " << epoch + 1 << "/" << epochs << "\n";
-
-        this->forward(input);
-
-        float loss = mse_loss(target, this->layers.back()->neurons);  // Output of the last layer is the prediction
-        std::cout << "Epoch: " << epoch << ", Loss: " << loss << "\n";
-
-        // Print loss to monitor progress
+    // Propagate the error backward through each layer
+    for (int i = this->layers.size() - 1; i >= 0; --i) {
+        dL_dO = this->layers[i]->backward(dL_dO);
     }
 }
+
+
+void NN::train(std::vector<std::vector<float>> &inputs, std::vector<std::vector<float>> &targets, int epochs) {
+    this->init_weights();
+
+    // Step 2: Training loop for specified epochs
+    for (int epoch = 0; epoch < epochs; ++epoch) {
+        float total_loss = 0.0f;
+
+        for (size_t i = 0; i < inputs.size(); ++i) {
+            std::vector<float> input = inputs[i];
+            std::vector<float> target = targets[i];
+
+            // Forward pass for each input-target pair
+            std::vector<float> out = this->forward(input);
+
+            // Calculate the loss
+            float loss = mse_loss(target, out);
+            total_loss += loss;
+
+            // Backward pass
+            this->backward(target);
+        }
+
+        // Output the loss for the current epoch
+        std::cout << "Epoch: " << epoch + 1 << "/" << epochs << ", Loss: " << total_loss / inputs.size() << "\n";
+    }
+}
+
 
 std::vector<float> NN::predict(std::vector<float> &input) {
     // Perform forward pass
